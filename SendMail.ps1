ce863d60ac7c255f6771b468c46489c773c5a720
vw6 <proshin@gismeteo.com>
2021-11-29 15:14:52 +0300

    Update SendMail.ps1

--- SendMail.ps1
+++ SendMail.ps1
@@ -1,14 +1,14 @@
 ﻿$configFile = "SendMail_config.json"
 $json = Get-Content $configFile | ConvertFrom-Json 
 #Приветствие
-Write-Host "Сервер: " -NoNewline -ForegroundColor Blue
+Write-Host "Server: " -NoNewline -ForegroundColor Blue
 Write-Host ($json.settings.serverSmtp + ":"+$json.settings.Port) -ForegroundColor Green
-Write-Host "Пользователь отправитель: " -NoNewline -ForegroundColor Blue
+Write-Host "User sender: " -NoNewline -ForegroundColor Blue
 Write-Host ($json.settings.MyName + " ("+$json.settings.Login+")") -ForegroundColor Green
-Write-Host "Путь для поиска: " -NoNewline -ForegroundColor Blue
+Write-Host "Search path: " -NoNewline -ForegroundColor Blue
 Write-Host ($json.settings.FilePath) -ForegroundColor Green
 Write-Host ("")
-$launchPermission = Read-Host("Начать рассылку? Yes/No")
+$launchPermission = Read-Host("Start mailing? Yes/No")
 Write-Host ("")
 if ($launchPermission -eq "Yes" -or $launchPermission -eq "Y") {
    #Создаем экземпляр класса подключения к SMTP серверу
@@ -27,13 +27,13 @@ if ($launchPermission -eq "Yes" -or $launchPermission -eq "Y") {
       $mes.Body = $json.settings.MailBody
       $mes.To.Add($user.MailTo)
       Write-Host($user.name) -ForegroundColor Blue -NoNewline
-      Write-Host ": Поиск вложений..."
+      Write-Host ": Search attachments ..."
       #Write-Host "Путь для поиска: "  -ForegroundColor Gray -NoNewline
       #Write-Host  $($json.settings.FilePath +'/'+ $user.name)
       Get-ChildItem -Path $($json.settings.FilePath +'/'+ $user.name) -Filter ($json.settings.FileMask) | where-object {
          ($_.LastWriteTime -ge (get-date).AddDays($json.settings.LastTimeAccessFile))}|
       ForEach-Object{
-         Write-Host "Файл для отправки: " -ForegroundColor Blue -NoNewline
+         Write-Host "File to send: " -ForegroundColor Blue -NoNewline
          Write-Host($_.FullName)
          $att = (New-object Net.Mail.Attachment( $_.FullName))
          $mes.Attachments.Add($att) 
@@ -42,14 +42,14 @@ if ($launchPermission -eq "Yes" -or $launchPermission -eq "Y") {
       #Отправляем письмо, освобождаем память
       $smtp.Send($mes)
       try{
-      Write-Host ("Сообщение отпрвлено пользователю: ")-ForegroundColor Blue -NoNewline
+      Write-Host ("Message sent to user: ")-ForegroundColor Blue -NoNewline
       write-host $user.name -ForegroundColor Green
       $att.Dispose()
       $mes.Dispose()
       write-host("========================")
       }
       catch{
-         Write-Host('Не было вложений') -ForegroundColor Red
+         Write-Host('There were no investments') -ForegroundColor Red
          write-host("========================")
 
       }
